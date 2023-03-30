c48aaac7c314e927d6683f19a1e93a144e6aa870
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-03-29 22:31:44 -0600

    commit tree 9bfc1201cb8b91b29a5f4498ddf6ba25e4a74ac8

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemons, setPokemons] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then(r=>r.json())
+    .then(setPokemons)
+  }, [])
+
+  const addPokemon = (newPokemon) => {
+    setPokemons([newPokemon, ...pokemons])
+  }
+
+  const pokemonToDisplay = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm addPokemon={addPokemon} />
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} onChangeSearch={setSearchTerm} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={pokemonToDisplay} />
     </Container>
   );
 }
